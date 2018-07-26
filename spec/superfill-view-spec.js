ccd295d71e8e3ec3b257751d887c6177fd174293
c0d3er <vimal+c0d3er@users.noreply.github.com>
2018-07-26 19:50:56 +0530

    React ES6 snippets added

--- spec/superfill-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SuperfillView from '../lib/superfill-view';
-
-describe('SuperfillView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
