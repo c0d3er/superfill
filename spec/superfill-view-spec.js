96bccc852b38ab576307bd7f1d1d83f4a568cd71
c0d3er <vimal+c0d3er@users.noreply.github.com>
2018-07-26 18:33:51 +0530

    source added

---
+++ spec/superfill-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SuperfillView from '../lib/superfill-view';
+
+describe('SuperfillView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
